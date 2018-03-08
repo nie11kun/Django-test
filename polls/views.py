63903fe66346f463cc092e1b76a0f2d731dc9147
Marco Nie <nie11kun@outlook.com>
2018-03-08 13:53:44 +0800

    working project

--- polls/views.py
+++ polls/views.py
@@ -1,17 +1,52 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, loader, get_object_or_404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
+
+from .models import Question, Choice
 
 # Create your views here.
+
 def index(request):
-    return HttpResponse("hello, you are at the poll index")
+    lastest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'lastest_question_list': lastest_question_list}
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):# detail(request=<HttpRequest object>, question_id=34)
-    return HttpResponse('you are looking at question %s.' % question_id)
-
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+    '''
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
+    '''
+    
 def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+    '''
     response = 'your are looking at question %s.'
     return HttpResponse(response % question_id)
+    '''
 
 def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "you did not select a choice",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+    '''
     return HttpResponse('you are voting on question %s.' % question_id)
+    '''
+
+    
 
