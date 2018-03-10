ee00d61f152dcdc67772d7e88b0f8260af861514
Marco Nie <nie11kun@outlook.com>
2018-03-10 11:13:55 +0800

    part6

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,76 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+import datetime
+from django.utils import timezone
+from django.urls import reverse
+
+from .models import Question
+
 
 # Create your tests here.
+
+def creat_question(question_text, days):
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
+
+class QuestionIndexViewTests(TestCase):
+    client = Client()
+    
+    def test_no_question(self):
+        response = self.client.get(reverse('polls:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'no polls are availble.')
+        self.assertQuerysetEqual(response.context['lastest_question_list'], [])
+
+    def test_past_question(self):
+        creat_question(question_text='past question', days=-30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(response.context['lastest_question_list'], ['<Question: past question>'])
+
+    def test_further_question(self):
+        creat_question(question_text='further question', days=30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertContains(response, 'no polls are availble.')
+        self.assertQuerysetEqual(response.context['lastest_question_list'], [])
+
+    def test_further_question_and_past_question(self):
+        creat_question(question_text='past question', days=-30)
+        creat_question(question_text='further question', days=30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(response.context['lastest_question_list'], ['<Question: past question>'])
+
+    def test_two_past_question(self):
+        creat_question(question_text='past question 1', days=-30)
+        creat_question(question_text='past question 2', days=-5)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(response.context['lastest_question_list'], ['<Question: past question 1>', '<Question: past question 2>'])
+
+class QuestionDetailViewTests(TestCase):
+    def test_future_question(self):
+        future_question = creat_question(question_text='future question', days=5)
+        url = reverse('poll:detail', args=future_question.id,)
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        past_question = creat_question(question_text='past question', days=-5)
+        url = reverse('poll:detail', args=past_question.id,)
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
+        
+    
+
+class QuestionModelTests(TestCase):
+    def test_was_published_recently_with_future_question(self):
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_older_question(self):
+        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        older_question = Question(pub_date=time)
+        self.assertIs(older_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_question(self):
+        time = timezone.now() - datetime.timedelta(hours=23)
+        recent_question = Question(pub_date=time)
+        self.assertIs(recent_question.was_published_recently(), True)
--- polls/views.py
+++ polls/views.py
@@ -1,35 +1,30 @@
-from django.shortcuts import render, loader, get_object_or_404
-from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Question, Choice
 
 # Create your views here.
 
-def index(request):
-    lastest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'lastest_question_list': lastest_question_list}
-    return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'lastest_question_list'# default is question_list
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())[:5]
 
-def detail(request, question_id):# detail(request=<HttpRequest object>, question_id=34)
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
-    '''
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("question does not exist")
-    return render(request, 'polls/detail.html', {'question': question})
-    '''
-    
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
-    '''
-    response = 'your are looking at question %s.'
-    return HttpResponse(response % question_id)
-    '''
+class DetailView(generic.DetailView):
+    model = Question
+    template_name='polls/detail.html'
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -44,9 +39,5 @@ def vote(request, question_id):
         selected_choice.votes += 1
         selected_choice.save()
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
-    '''
-    return HttpResponse('you are voting on question %s.' % question_id)
-    '''
 
-    
 
