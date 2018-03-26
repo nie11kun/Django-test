67772fc6921f6d0f327dd3dcbe826725a32d90ac
Marco Nie <nie11kun@outlook.com>
2018-03-27 00:12:56 +0800

    update path in app url

--- polls/urls.py
+++ polls/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 app_name = 'polls'
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
-    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
-    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
-    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('django/polls/', views.IndexView.as_view(), name='index'),
+    path('django/polls/<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('django/polls/<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('django/polls/<int:question_id>/vote/', views.vote, name='vote'),
 ]
